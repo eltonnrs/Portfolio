3608f0520e601fb1addda6e81695911a37164b9a
Elton <eltonnrs25@gmail.com>
2021-08-04 15:19:52 -0300

    Changing sidebar buttons logic and responsive design choices
    Added portfolio projects to the skills section, redesigned responsive queries and deleted some buttons. Also, changed js code to make it more clean.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 const menuBtns = document.getElementsByClassName("btn--menu");
 let activeSection = document.getElementById("home");
 for (let button of menuBtns) {
-    button.addEventListener("click", (evt) => {
-        let clicked = document.getElementById(evt.target.innerText.toLowerCase());
-        /*
+  button.addEventListener("click", (evt) => {
+    let clicked = document.getElementById(evt.target.innerText.toLowerCase());
+    console.log()
+    /*
             If the clicked button is already active, don't change the active state
          */
-        if (clicked === activeSection) {
-            console.log("Active button clicked");
-        } else {
-            activeSection.classList.add("inactive");
-            activeSection.classList.remove("active");
-            clicked.classList.add("active")
-            clicked.classList.remove("inactive");
-            activeSection = clicked;
-        }
-    });
+    if (clicked === activeSection) {
+      console.log("Active button clicked");
+    } else {
+      activeSection.classList.add("inactive");
+      activeSection.classList.remove("active");
+      clicked.classList.add("active");
+      clicked.classList.remove("inactive");
+      activeSection = clicked;
+    }
+  });
 }
